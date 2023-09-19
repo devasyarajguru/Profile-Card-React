bd76acd8e92da78237cf846b5c1a022712a0e891
Devasya <devasyarajguru@gmail.com>
2023-09-19 17:55:19 +0530

    Profile-card

---
+++ profile-card/src/App.jsx
@@ -0,0 +1,13 @@
+// import React from "react"
+import Header from './Components/Header';
+export default function App()
+{
+    return (
+      <>
+        <h1>Dev</h1>
+          <div className="app-container">
+              <Header />  
+              </div>
+      </>
+    )
+}
