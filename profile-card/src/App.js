16ee6148045e4b5d89f4bcc1203a12bde20bfb03
Devasya <devasyarajguru@gmail.com>
2023-10-01 12:04:45 +0530

    Profile-card

--- profile-card/src/App.js
+++ profile-card/src/App.js
@@ -1,8 +1,8 @@
 // import React from "react"
-import Header from './Components/Header';
+import Header from './Components/Header.js';
 import './App.css';
-import Main from './Components/Main';
-import Footer from './Components/Footer';
+import Main from './Components/Main.js';
+import Footer from './Components/Footer.js';
 export default function App()
 {
     return (
