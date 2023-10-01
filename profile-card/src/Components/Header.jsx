481b597629f180a2c81d3ee07faf49675480e787
Devasya <devasyarajguru@gmail.com>
2023-10-01 11:51:40 +0530

    Profile-card

--- profile-card/src/Components/Header.jsx
+++ profile-card/src/Components/Header.jsx
@@ -1,11 +1,11 @@
-import profile from "../images/img1.jpg";
+import profile from "../images/a.jpeg";
 export default function Header()
 
 {
     return(
         <>
             <div className="header-container">
-                <img src={profile} alt="image" width="500px" height="300px"></img>
+                <img src={profile} alt="image" width="500px" height="400px"></img>
             </div>
         </>
     )
